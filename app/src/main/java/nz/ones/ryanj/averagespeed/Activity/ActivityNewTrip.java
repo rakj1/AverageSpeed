5fd86e7057d6588d02bf3ee04258104f3c3aaa4a
Ryan Jones <rakj1@students.waikato.ac.nz>
2016-03-11 15:23:26 +1300

    Added working GPS code

--- app/src/main/java/nz/ones/ryanj/averagespeed/Activity/ActivityNewTrip.java
+++ app/src/main/java/nz/ones/ryanj/averagespeed/Activity/ActivityNewTrip.java
@@ -2,203 +2,127 @@ package nz.ones.ryanj.averagespeed.Activity;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-
-import java.util.Calendar;
-import java.util.Date;
-
-import nz.ones.ryanj.averagespeed.DataObjects.Point;
-import nz.ones.ryanj.averagespeed.DataObjects.Trip;
-import nz.ones.ryanj.averagespeed.DatabaseHandler;
 import nz.ones.ryanj.averagespeed.R;
 import nz.ones.ryanj.averagespeed.Util.Constants;
 
 import static android.util.Log.d;
 
-public class ActivityNewTrip extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+public class ActivityNewTrip extends AppCompatActivity implements LocationListener {
 
     private final String DEBUG_TAG = "AverageSpeed." + getClass().getCanonicalName();
-    final Handler h = new Handler();
 
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
-
-    private Trip currentTrip;
-    private long tripId;
+    private TextView latitude;
+    private TextView longitude;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_new_trip);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        latitude = (TextView) findViewById(R.id.textViewLat);
+        longitude = (TextView) findViewById(R.id.textViewLong);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // Final may break this
-        final DatabaseHandler db = new DatabaseHandler(getBaseContext());
 
-        /********Boring UI binding********/
-        /*End Trip Button*/
-        Button endButton = (Button) findViewById(R.id.buttonEndTrip);
-        endButton.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-                h.removeCallbacksAndMessages(null);
-                endTrip();
-            }
-        });
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-
-        //Create a new trip and add to the db getting the ID of the trip
-        String tripName = "trip: " + Calendar.getInstance().getTime().toString();
-        d(DEBUG_TAG, "Starting trip \"" + tripName + "\" and adding to Database");
-        Point startingPoint = getCurrentPoint();
-        currentTrip = new Trip(tripName, startingPoint);
-
-        tripId = db.addTrip(currentTrip);
-        h.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                //Get co-ordinates and time to add it as a point
-                h.postDelayed(this, Constants.TIME_INTERVAL);
-                addPoint(db);
-            }
-        }, Constants.TIME_INTERVAL);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            d(DEBUG_TAG, "Can't get GPS permission");
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            d(DEBUG_TAG, "Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            latitude.setText("Location not available");
+            longitude.setText("Location not available");
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mGoogleApiClient.connect();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(provider, Constants.TIME_INTERVAL, 20, this);
     }
 
+    /* Remove the locationlistener updates when Activity is paused */
     @Override
     protected void onPause() {
         super.onPause();
-        if (mGoogleApiClient.isConnected()) {
-            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            mGoogleApiClient.disconnect();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        locationManager.removeUpdates(this);
     }
 
     @Override
-    public void onBackPressed() {
-        d(DEBUG_TAG, "User pressed back button");
-        // Double check the user wants to exit
-        new AlertDialog.Builder(this)
-                .setTitle("End Trip")
-                .setMessage("Do you really want to end this trip?")
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        endTrip();
-                    }
-                })
-                .setNegativeButton(android.R.string.no, null).show();
+    public void onLocationChanged(Location location) {
+        float lat = (float) (location.getLatitude());
+        float lng = (float) (location.getLongitude());
+        latitude.setText(String.valueOf(lat));
+        longitude.setText(String.valueOf(lng));
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            error("Don't have permission to use GPS");
-        }
-        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (location == null)
-            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-        d(DEBUG_TAG, "Location services connected");
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
     }
 
     @Override
-    public void onConnectionSuspended(int i)
-    {
-        d(DEBUG_TAG, "Location services suspended. Please reconnect");
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
-    public void onLocationChanged(Location location)
-    {
-        addPoint(new DatabaseHandler(getBaseContext()));
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult result)
-    {
-
-    }
-
-    private void addPoint(DatabaseHandler databaseHandler)
-    {
-        d(DEBUG_TAG, "Trip " + tripId + ": Getting current point and adding to Database");
-        Point p = getCurrentPoint();
-        databaseHandler.addPoint(new Point(tripId, p.Time(), p.Longitude(), p.Latitude()));
-        currentTrip.addPoint(p);
-    }
-
-    public void endTrip() {
-        final DatabaseHandler db = new DatabaseHandler(getBaseContext());
-        d(DEBUG_TAG, "Ending trip:" + currentTrip.ID() + ": Getting last point and adding to database");
-        Point p = getCurrentPoint();
-        db.addPoint(new Point(tripId, p.Time(), p.Longitude(), p.Latitude()));
-        currentTrip = db.getTrip(tripId);
-        currentTrip.endTrip(p);
-        db.updateTrip(currentTrip);
-        finish();
-    }
-
-    public Point getCurrentPoint() {
-        Date startTime = (Calendar.getInstance().getTime());
-
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                error("Cannot get last location. Cannot start trip");
-                finish();
-                return null;
-            }
-        }
-        mLocationRequest = LocationRequest.create()
-                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-                .setInterval(Constants.TIME_INTERVAL)
-                .setFastestInterval(Constants.TIME_INTERVAL);
-
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-
-        Double latitude = location.getLatitude();
-        Double longitude = location.getLongitude();
-        Point point = new Point(startTime, latitude, longitude);
-
-        return point;
-    }
-
-    private void error(String errorMessage)
-    {
-        d(DEBUG_TAG, errorMessage);
-        finish();
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
     }
 }
